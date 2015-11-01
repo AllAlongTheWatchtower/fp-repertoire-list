e3321c9eb6b040c4c70d16ada00b1f5fc5d21cbe
Valerie Hurwitz <vhurwitz@bsu.edu>
2015-10-31 22:17:40 -0400

    Update URL Factory class (small update)

--- src/edu/bsu/cs222/fp/repertoireList/URLFactory.java
+++ src/edu/bsu/cs222/fp/repertoireList/URLFactory.java
@@ -11,7 +11,7 @@ public class URLFactory {
 		try {
 			encodedQuery = URLEncoder.encode(query, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
-			throw new RuntimeException("message");
+			new ExceptionHandler("System error!  Please try again.");
 		}
 		url =  "http://developer.echonest.com/api/v4/song/search?api_key=NDVFILMAVOOY8ITWS&artist=" + encodedQuery + "&format=xml&results=100";
 	}
