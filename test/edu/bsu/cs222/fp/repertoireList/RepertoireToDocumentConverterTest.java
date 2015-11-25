8b0053ee550bd6681f073eb76f198d6ee8688a4c
Valerie Hurwitz <vhurwitz@bsu.edu>
2015-11-24 21:59:15 -0500

    Clean-up

--- test/edu/bsu/cs222/fp/repertoireList/RepertoireToDocumentConverterTest.java
+++ test/edu/bsu/cs222/fp/repertoireList/RepertoireToDocumentConverterTest.java
@@ -36,25 +36,6 @@ public class RepertoireToDocumentConverterTest {
 	public void testEquivalentDocuments() {
 		RepertoireToDocumentConverter converter2 = new RepertoireToDocumentConverter(testRep);
 		Document duplicate = converter2.getDocument();
-		/** try {
-			printDocument(duplicate, System.out);
-			printDocument(document, System.out);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} */
 		assertTrue(duplicate.isEqualNode(document));
-	}
-	
-	public static void printDocument(Document doc, OutputStream out) throws IOException, TransformerException {
-	    TransformerFactory tf = TransformerFactory.newInstance();
-	    Transformer transformer = tf.newTransformer();
-	    transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
-	    transformer.setOutputProperty(OutputKeys.METHOD, "xml");
-	    transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-	    transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
-	    transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
-
-	    transformer.transform(new DOMSource(doc), 
-	         new StreamResult(new OutputStreamWriter(out, "UTF-8")));
 	}
 }
